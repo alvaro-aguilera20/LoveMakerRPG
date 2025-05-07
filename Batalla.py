1b394210f2c3aefbe459bdbdc461dd94ae3bda4f
alvaro-aguilera20 <166621119+alvaro-aguilera20@users.noreply.github.com>
2025-05-07 19:45:47 -0400

    "mejoras GUI"
    hicimos del menu algo mas intuitivo, cosa que pudieras ver en todo momento a tu equipo y pudieras ver mejor que opcion elijes al parpadear cuando pasas el mouse encima

--- Batalla.py
+++ Batalla.py
@@ -11,6 +11,10 @@ pygame.font.Font()
 
 #definir graficos que sacamos de la carpeta graficos
 
+humano = pygame.image.load("graficos/Humano.png")
+waffle = pygame.image.load("graficos/Bot1.png")
+mandy = pygame.image.load("graficos/bot2.png")
+
 #definir los colores que usaremos
 negro = (0,0,0)
 rojo = (255,0,0)
@@ -25,6 +29,7 @@ oxido = (183,65,14)
 rosaOscuro = (218,44,67)
 celeste = (81,209,246)
 celestePastel = (121,210,230)
+amarillo = (253,253,150)
 
 #resolucion de la pantalla
 pantalla=pygame.display.set_mode((1280,720))
@@ -37,21 +42,105 @@ FTimes = pygame.font.SysFont("times",12)
 FPapyro = pygame.font.SysFont("Papyrus", 12)
 FCalacaChida = pygame.font.SysFont("comic sans", 12)
 FOCR = pygame.font.SysFont("OCR A EXTENDED",100)
+FOCRM = pygame.font.SysFont("OCR A EXTENDED",50)
 #definir mostrar texto (necesita el texto que quieres meter, la fuente ya definida, el color definido o no, y las coordenadas X e Y)
 
 def mostrarTexto(texto, fuente, color, X, Y):
     Text= fuente.render(texto, True, color)
     pantalla.blit(Text,(X,Y))
 
+#definir iconos del equipo
+
+class boton():
+    def __init__(BE,imagen, x, y,font,texto):
+        BE.imagen = imagen
+        BE.x = x
+        BE.y = y
+        BE.texto = texto
+        BE.font = font
+        BE.text = BE.font.render(BE.texto, True, celeste)
+        if imagen is None:
+            BE.imagen = BE.text
+        BE.rect = BE.imagen.get_rect(center=(BE.x,BE.y))
+        BE.text_rect = BE.text.get_rect(center=(BE.x, BE.y))
+        
+    def update(BE):
+        if BE.imagen is not None:
+            pantalla.blit(BE.imagen,BE.rect)
+        pantalla.blit(BE.text, BE.text_rect)
+
+    def botonApretado(BE, position):
+        if position[0] in range(BE.rect.left, BE.rect.right) and position[1] in range(BE.rect.top, BE.rect.bottom):
+            return True
+        return False
+
+    def cambiarDeColor(BE, posicion):
+        if posicion[0] in range(BE.text_rect.left, BE.text_rect.right) and posicion[1] in range(BE.text_rect.top, BE.text_rect.bottom):
+            BE.text = BE.font.render(BE.texto, True, plateado)
+        else:
+            BE.text = BE.font.render(BE.texto, True, celeste)
+    
+
+humano_surface = pygame.transform.scale(humano,(110,110))
+bot1_surface = pygame.transform.scale(waffle,(110,110))
+bot2_surface = pygame.transform.scale(mandy,(110,110))
+
+iconoHumano1 = boton(humano_surface, 85, 625,FOCR,"")
+iconoBot1 = boton(bot1_surface, 290, 625,FOCR,"")
+iconoBot2 = boton(bot2_surface, 495, 625,FOCR,"")
+iconoHumano4 = boton(humano_surface, 700, 625,FOCR,"")
+iconoHumano5 = boton(humano_surface, 905, 625,FOCR,"")
+iconoHumano6 = boton(humano_surface, 1110, 625,FOCR,"")
+
+accederMenuMovimientosPersonaje1 = boton(None, 225, 450,FOCR,"ATACAR")
+accederMenuObjetos = boton(None, 875, 450,FOCR,"Objetos")
+#definir busqueda por base de datos
+
+def obtenerObjeto(id):
+    conn = sql.connect("Registro.db")
+    cursor = conn.execute("SELECT nombreObjeto FROM objetos where objetoID = ?",
+        (id,))
+    for row in cursor:
+        return(row[0])
+    
+def obtenerMovimiento(id):
+    conn = sql.connect("Registro.db")
+    cursor = conn.execute("SELECT nombreMovimiento FROM movimientos where movimientoID = ?",
+        (id,))
+    for row in cursor:
+        return(row[0]) 
 
 #con este comando la pantalla seguira abierta hasta que la cerremnos manualmente o se acabe el juego
 while True:
+    posicionMouse = pygame.mouse.get_pos()
+
     pantalla.fill(negro)
+    mandyOponente = pygame.transform.scale(mandy,(600,400))
+    pantalla.blit(mandyOponente,(350,0))
     pygame.draw.rect(pantalla,(negro), (0,360,1280,360),border_top_left_radius=25,border_top_right_radius=25)
     pygame.draw.rect(pantalla,(azul), (0,360,1280,360), width= 25,border_radius=25)
-    mostrarTexto("ATACAR",FOCR,celeste, 200, 400)
-    mostrarTexto("OBJETOS",FOCR,celeste, 700, 400)
-    mostrarTexto("EQUIPO",FOCR,celeste, 500, 550)
+    accederMenuMovimientosPersonaje1.cambiarDeColor(posicionMouse)
+    accederMenuMovimientosPersonaje1.update()
+    accederMenuObjetos.cambiarDeColor(posicionMouse)
+    accederMenuObjetos.update()
+    iconoHumano1.update()
+    iconoBot1.update()
+    iconoBot2.update()
+    iconoHumano4.update()
+    iconoHumano5.update()
+    iconoHumano6.update()
+    pygame.draw.rect(pantalla,(rosaOscuro),(150, 600, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(355, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(355, 650, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(560, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(560, 650, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(765, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(765, 650, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(970, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(970, 650, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(1175, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(1175, 650, 75,20))
+
     pygame.display.update()
 
     for event in pygame.event.get():
--- CodigosPrueba.py
+++ CodigosPrueba.py
@@ -10,7 +10,9 @@ pygame.font.Font()
 cielo = pygame.image.load("graficos/cielo.png")
 edificiosDeFondo = pygame.image.load("graficos/edificios.png")
 jefecitoEnfadadito = pygame.image.load("graficos/jefe.png")
+humano = pygame.image.load("graficos/Humano.png")
 
+#definir colores
 negro = (0,0,0)
 rojo = (255,0,0)
 verde_lima = (70,255,0)
@@ -24,9 +26,12 @@ oxido = (183,65,14)
 rosaOscuro = (218,44,67)
 celeste = (81,209,246)
 celestePastel = (121,210,230)
+amarillo = (253,253,150)
 
 pantalla=pygame.display.set_mode((1280,720))
 pygame.display.set_caption("ventana basica")
+
+#definir fuentes tipograficas
 
 FArial = pygame.font.SysFont("Arial", 50)
 FTimes = pygame.font.SysFont("times",100)
@@ -34,10 +39,54 @@ FPapyro = pygame.font.SysFont("Papyrus", 50)
 FCalacaChida = pygame.font.SysFont("comic sans", 50)
 Flucida = pygame.font.SysFont("lucida sans typewritter",100)
 FOCR = pygame.font.SysFont("OCR A EXTENDED",100)
+FOCRM = pygame.font.SysFont("OCR A EXTENDED",50)
 
 def mostrarTexto(texto, fuente, color, X, Y):
     Text= fuente.render(texto, True, color)
     pantalla.blit(Text,(X,Y))
+
+class boton():
+    def __init__(BE,imagen, x, y,font,texto):
+        BE.imagen = imagen
+        BE.x = x
+        BE.y = y
+        BE.texto = texto
+        BE.font = font
+        BE.text = BE.font.render(BE.texto, True, celeste)
+        if imagen is None:
+            BE.imagen = BE.text
+        BE.rect = BE.imagen.get_rect(center=(BE.x,BE.y))
+        BE.text_rect = BE.text.get_rect(center=(BE.x, BE.y))
+        
+    def update(BE):
+        if BE.imagen is not None:
+            pantalla.blit(BE.imagen,BE.rect)
+        pantalla.blit(BE.text, BE.text_rect)
+
+    def botonApretado(BE, position):
+        if position[0] in range(BE.rect.left, BE.rect.right) and position[1] in range(BE.rect.top, BE.rect.bottom):
+            return True
+        return False
+
+    def cambiarDeColor(BE, posicion):
+        if posicion[0] in range(BE.text_rect.left, BE.text_rect.right) and posicion[1] in range(BE.text_rect.top, BE.text_rect.bottom):
+            BE.text = BE.font.render(BE.texto, True, plateado)
+        else:
+            BE.text = BE.font.render(BE.texto, True, celeste)
+    
+    
+
+humano_surface = pygame.transform.scale(humano,(110,110))
+
+#iconoHumano1 = botonEquipo(humano_surface, 85, 625)
+#iconoHumano2 = botonEquipo(humano_surface, 290, 625)
+#iconoHumano3 = botonEquipo(humano_surface, 495, 625)
+#iconoHumano4 = botonEquipo(humano_surface, 700, 625)
+#iconoHumano5 = botonEquipo(humano_surface, 905, 625)
+#iconoHumano6 = botonEquipo(humano_surface, 1110, 625)
+
+
+
 
 def obtenerObjeto(id):
     conn = sql.connect("Registro.db")
@@ -45,7 +94,15 @@ def obtenerObjeto(id):
         (id,))
     for row in cursor:
         return(row[0])
-    conn.close()
+    
+def obtenerMovimiento(id):
+    conn = sql.connect("Registro.db")
+    cursor = conn.execute("SELECT nombreMovimiento FROM movimientos where movimientoID = ?",
+        (id,))
+    for row in cursor:
+        return(row[0])
+    
+#botonOpcion_surface = pygame.transform.scale()
 
 while True:
     #pantalla.fill(azul)
@@ -64,29 +121,43 @@ while True:
     #menu de movimientos
     #pygame.draw.rect(pantalla,(negro), (0,360,1280,360),border_top_left_radius=25,border_top_right_radius=25)
     #pygame.draw.rect(pantalla,(azul), (0,360,1280,360), width= 25,border_radius=25)
-    #mostrarTexto("PATADA",FOCR,celeste, 50, 400)
-    #mostrarTexto("MOV2",FOCR,celeste, 650, 400)
-    #mostrarTexto("MOV3",FOCR,celeste, 50, 550)
-    #mostrarTexto("MOV4",FOCR,celeste, 650, 550)
-    #mostrarTexto("<-",FOCR,celestePastel, 400, 400)
+    #mostrarTexto(obtenerMovimiento(2),FOCRM,celeste, 625, 385)
+    #mostrarTexto(obtenerMovimiento(3),FOCRM,celeste, 50, 450)
+    #mostrarTexto(obtenerMovimiento(4),FOCRM,celeste, 625, 450)
+    #mostrarTexto("<--",FOCRM,(celeste),50,525)
+    #mostrarTexto("-->",FOCRM,(celeste),1150,525)
+
 
     #menu de objetos
-    pygame.draw.rect(pantalla,(negro), (0,360,1280,360),border_top_left_radius=25,border_top_right_radius=25)
-    pygame.draw.rect(pantalla,(azul), (0,360,1280,360), width= 25,border_radius=25)
-    mostrarTexto(obtenerObjeto(1),FOCR,celeste, 50, 400)
-    mostrarTexto(obtenerObjeto(5),FOCR,celeste, 650, 400)
-    mostrarTexto(obtenerObjeto(3),FOCR,celeste, 50, 550)
-    mostrarTexto(obtenerObjeto(4),FOCR,celeste, 650, 550)
-    #mostrarTexto("<-",FOCR,celestePastel, 400, 400)
-    pygame.display.update()
-
-    #menu de equipo
     #pygame.draw.rect(pantalla,(negro), (0,360,1280,360),border_top_left_radius=25,border_top_right_radius=25)
     #pygame.draw.rect(pantalla,(azul), (0,360,1280,360), width= 25,border_radius=25)
-    #mostrarTexto("TU",FOCR,celeste, 50, 400)
-    #mostrarTexto("BOT",FOCR,celeste, 650, 400)
-    #mostrarTexto("<-",FOCR,celestePastel, 400, 400)
-    #pygame.display.update()
+    #mostrarTexto(obtenerObjeto(1),FOCRM,celeste, 50, 385)
+    #mostrarTexto(obtenerObjeto(5),FOCRM,celeste, 350, 385)
+    #mostrarTexto(obtenerObjeto(3),FOCRM,celeste, 650, 385)
+    #mostrarTexto(obtenerObjeto(4),FOCRM,celeste, 950, 385)
+    #mostrarTexto(obtenerObjeto(6),FOCRM,celeste, 250, 485)
+    #mostrarTexto(obtenerObjeto(8),FOCRM,celeste, 750, 485)
+    #iconoHumano1.update()
+    #iconoHumano2.update()
+    #iconoHumano3.update()
+    #iconoHumano4.update()
+    #iconoHumano5.update()
+    #iconoHumano6.update()
+    pygame.draw.rect(pantalla,(rosaOscuro),(150, 600, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(355, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(355, 650, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(560, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(560, 650, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(765, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(765, 650, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(970, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(970, 650, 75,20))
+    pygame.draw.rect(pantalla,(rosaOscuro),(1175, 600, 75,20))
+    pygame.draw.rect(pantalla,(amarillo),(1175, 650, 75,20))
+
+
+    #mostrarTexto("<-",FOCRM,celestePastel, 250, 400)
+    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
