1a15b4f20c32fb402f876d0e3a416eca8026c9c3
alvaro-aguilera20 <166621119+alvaro-aguilera20@users.noreply.github.com>
2025-05-28 19:25:46 -0400

    simplificacion de menus y añadi a shadowface
    tuve algunas quejas de como funcionaba el menu de movimientos por parte de mis compañeros asi que lo hice mas facil de editar cosa que ellos puedan hacer lo que quieran con el menu mientras los movimientos esten dentro de la base de datos y coloquen 0 para indicar que esta vacio, tambien añadi los fondos hechos por benjamin y a black junto a shadowface, falta hacer su forma escondida pero creo que eso puede esperar

--- Batalla.py
+++ Batalla.py
@@ -41,11 +41,15 @@ humano1 = pygame.image.load("graficos/Humano1.png")
 
 humano2 = pygame.image.load("graficos/Humano2.png")
 
+humano3 = pygame.image.load("graficos/Humano3.png")
+
 Bot1 = pygame.image.load("graficos/Bot1.png")
 
 Bot2 = pygame.image.load("graficos/Bot2.png")
 
 Bot3 = pygame.image.load("graficos/Bot3.png")
+
+Bot4 = pygame.image.load("graficos/Bot4.png")
 
 Bella = pygame.image.load("graficos/Bella.png")
 
@@ -53,13 +57,19 @@ Angel = pygame.image.load("graficos/Angel.png")
 
 Glitter = pygame.image.load("graficos/Glitter.png")
 
+Black = pygame.image.load("graficos/Black.png")
+
 Miguel = pygame.image.load("graficos/Miguel.png")
 
 Carlos = pygame.image.load("graficos/Carlos.png")
 
+Shadowfave = pygame.image.load("graficos/Shadowface.png")
+
 MontañitasFondo = pygame.image.load("graficos/montañitas.png")
 
-CiudadFondo= pygame.image.load("graficos/ciudad fondo.png")
+PasilloFondo = pygame.image.load("graficos/pasilloFondo.png")
+
+SinLuz = pygame.image.load("graficos/FueraDeServicioFondo.png")
 
 
 
@@ -264,7 +274,7 @@ class boton():
 
 
 
-
+#definir lo que seleccionaremos como iconos
 
 humano_surface = pygame.transform.scale(humano1,(110,110))
 
@@ -275,20 +285,24 @@ bot2_surface = pygame.transform.scale(Bot2,(110,110))
 
 bot3_surface = pygame.transform.scale(Bot3,(110,110))
 
+bot4_surface = pygame.transform.scale(Bot4,(110,110))
 
-fondo1 = pygame.transform.scale(MontañitasFondo,(1280,720))
-
-fondo2 = pygame.transform.scale(CiudadFondo,(1280,720))
-
+bot5_surface = pygame.transform.scale(humano3,(110,110))
 
 
 
-
-pantalla.blit(fondo2,(0,-300))
-
-
+#definir fondo
+fondo1 = pygame.transform.scale(SinLuz,(1280,720))
 
 
+
+#aplicar fondo
+
+pantalla.blit(fondo1,(0,-100))
+
+
+
+#activar los iconos de los personajes que usaremos mas adelante
 
 iconoHumano1 = boton(humano_surface, 85, 625,FOCR,"")
 
@@ -302,14 +316,14 @@ iconoBot2 = boton(bot2_surface, 495, 625,FOCR,"")
 iconoBot3 = boton(bot3_surface, 700, 625,FOCR,"")
 
 
-iconoBot4 = boton(bot1_surface, 905, 625,FOCR,"")
+iconoBot4 = boton(bot4_surface, 905, 625,FOCR,"")
 
 
-iconoBot5 = boton(bot1_surface, 1110, 625,FOCR,"")
+iconoBot5 = boton(bot5_surface, 1110, 625,FOCR,"")
 
 
 
-
+#definir opciones para el menu de humano
 
 accederMenuMovimientosPersonaje1 = boton(None, 225, 450,FOCR,"ATACAR")
 
@@ -364,58 +378,6 @@ def obtenerMovimiento(id):
 
     
 
-
-#definir objetos del inventario
-
-
-    
-
-
-Objeto1 = boton(None, 125, 425, FOCRM, obtenerObjeto(1))
-
-
-Objeto2 = boton(None, 425, 425, FOCRM, obtenerObjeto(3))
-
-
-Objeto3 = boton(None, 725, 425, FOCRM, obtenerObjeto(4))
-
-
-Objeto4 = boton(None, 1025, 425, FOCRM, obtenerObjeto(6))
-
-
-Objeto5 = boton(None, 450, 500, FOCRM, obtenerObjeto(8))
-
-
-Objeto6 = boton(None, 900, 500, FOCRM, obtenerObjeto(9))
-
-
-
-
-
-#definir movimientos
-
-
-
-
-
-movimiento1 = boton(None, 300, 420, FOCRM, obtenerMovimiento(2))
-
-
-movimiento2 = boton(None, 900, 420, FOCRM, obtenerMovimiento(3))
-
-
-movimiento3 = boton(None, 300, 480, FOCRM, obtenerMovimiento(5))
-
-
-movimiento4 = boton(None, 900, 480, FOCRM, obtenerMovimiento(6))
-
-
-movimiento5 = boton(None, 900, 480, FOCRM, obtenerMovimiento(7))
-
-
-
-
-
 #definir flechas de navegacion
 
 
@@ -436,7 +398,24 @@ def inventario():
 
     while True:
 
+        Objeto1 = boton(None, 125, 425, FOCRM, obtenerObjeto(1))
 
+
+        Objeto2 = boton(None, 425, 425, FOCRM, obtenerObjeto(3))
+
+
+        Objeto3 = boton(None, 725, 425, FOCRM, obtenerObjeto(4))    
+
+
+        Objeto4 = boton(None, 1025, 425, FOCRM, obtenerObjeto(6))
+
+
+        Objeto5 = boton(None, 450, 500, FOCRM, obtenerObjeto(8))    
+
+
+        Objeto6 = boton(None, 900, 500, FOCRM, obtenerObjeto(9))
+        
+        
         posicionMouse = pygame.mouse.get_pos()
 
 
@@ -574,7 +553,16 @@ def MenuMovimientos1():
 
     while True:
 
+        movimiento1 = boton(None, 300, 420, FOCRM, obtenerMovimiento(2))
 
+
+        movimiento2 = boton(None, 900, 420, FOCRM, obtenerMovimiento(3))
+
+
+        movimiento3 = boton(None, 300, 480, FOCRM, obtenerMovimiento(5))
+
+
+        movimiento4 = boton(None, 900, 480, FOCRM, obtenerMovimiento(6))
 
 
 
@@ -727,6 +715,19 @@ def MenuMovimientos2():
 
     while True:
 
+        movimiento1 = boton(None, 300, 420, FOCRM, obtenerMovimiento(0))
+
+
+        movimiento2 = boton(None, 900, 420, FOCRM, obtenerMovimiento(0))
+
+
+        movimiento3 = boton(None, 300, 480, FOCRM, obtenerMovimiento(0))
+
+
+        movimiento4 = boton(None, 900, 480, FOCRM, obtenerMovimiento(7))
+
+
+
         posicionMouse = pygame.mouse.get_pos()
 
 
@@ -735,12 +736,28 @@ def MenuMovimientos2():
 
         pygame.draw.rect(pantalla,(azul), (0,360,1280,360), width= 25,border_radius=25)
 
+        movimiento1.cambiarDeColor(posicionMouse)
 
-        movimiento5.cambiarDeColor(posicionMouse)
+
+        movimiento1.update()
 
 
-        movimiento5.update()
+        movimiento2.cambiarDeColor(posicionMouse)
 
+
+        movimiento2.update()
+
+
+        movimiento3.cambiarDeColor(posicionMouse)
+
+
+        movimiento3.update()
+
+
+        movimiento4.cambiarDeColor(posicionMouse)
+
+
+        movimiento4.update()
 
         FlechaI.cambiarDeColor(posicionMouse)
 
@@ -867,17 +884,27 @@ def MenuHumano():
 
         posicionMouse = pygame.mouse.get_pos()
 
-        botOponente = pygame.transform.scale(Angel,(500,450))
-
-        humanoOponente = pygame.transform.scale(Carlos,(150,300))
-
-        pantalla.blit(botOponente,(350,-25))
-
-        pantalla.blit(humanoOponente,(350,100))
 
         pygame.draw.rect(pantalla,(negro), (0,360,1280,360),border_top_left_radius=25,border_top_right_radius=25)
 
+
         pygame.draw.rect(pantalla,(azul), (0,360,1280,360), width= 25,border_radius=25)
+
+
+        #pygame.draw.rect(pantalla,(celestePastel), (400,200,300,300), width= 100,border_radius=100)
+
+
+        botOponente = pygame.transform.scale(Bella,(300,400))
+
+
+        humanoOponente = pygame.transform.scale(Miguel,(300,300))
+
+
+        pantalla.blit(humanoOponente,(550,100))
+
+
+        pantalla.blit(botOponente,(500,0))
+
 
         pygame.draw.rect(pantalla,(negro), (0,360,1280,360),border_top_left_radius=25,border_top_right_radius=25)
 
